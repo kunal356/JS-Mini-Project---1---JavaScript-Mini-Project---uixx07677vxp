6a1092a74d23af376ced92a6640196f18925acd5
Kunal <gaikwadkunal610@gmail.com>
2022-06-16 19:01:56 +0530

    final submission

--- App.js
+++ App.js
@@ -1,1 +1,53 @@
+// GET https://icanhazdadjoke.com/
 
+
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+//using promises
+// const generateJokes = ()=>{
+
+//     const setHeader={
+//         headers:{
+//             Accept : "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com/',setHeader)
+//     .then((res) =>  res.json() )
+//     .then((data)=>jokes.innerHTML = data.joke)
+//     .catch((error) => console.log(error))
+// }
+
+// using async await
+// how to convert funtion to async function
+// async function functionName(){
+    
+// }
+    // const fatfunction = async ()=>{
+
+// }
+        
+// using async await
+
+const generateJokes = async()=>{
+    try{
+        const setHeader={
+            headers:{
+                Accept : "application/json"
+            }
+        }
+        
+        const res = await  fetch('https://icanhazdadjoke.com/',setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }catch(err){
+        console.log(`The error is ${err}`)
+    }
+
+        
+    }
+
+
+jokeBtn.addEventListener('click',generateJokes)
+generateJokes();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# html-css-js-project-boilerplate
+# Best Joke Generator
+*Want to read best jokes on the internet??
+->Best Joke Generator generates a new joke whenever the button is clicked. HTML CSS and Javascript is used to make this project. I have also used api https://icanhazdadjoke.com/ for this project.
